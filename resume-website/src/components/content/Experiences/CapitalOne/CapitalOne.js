7f9f4a28b83b37a3ef0524bada67f4b93f1b8709
Ermal Dedej <dedej33@gmail.com>
2024-09-04 22:31:18 -0400

    Add about me section

--- resume-website/src/components/content/Experiences/CapitalOne/CapitalOne.js
+++ resume-website/src/components/content/Experiences/CapitalOne/CapitalOne.js
@@ -5,13 +5,14 @@ const CapitalOne = () => {
   return (
     <div>
       <h3 className="experience-name" >Capital One - McLean VA</h3>
-      <p><strong>Principal Software Engineer, June 2024 – Present</strong><br/>
-      Leading the design, development, and maintenance of digital payment systems. Skilled in code review, system optimization, and project management. Implementing best practices to ensure secure and scalable solutions while mentoring engineering teams and collaborating with stakeholders.</p>
+      <p><strong>Lead Software Engineer, June 2024 – Present</strong><br/>
+      Experienced Lead Software Engineer specializing in backend services, with a proven track record of designing, developing, and maintaining secure, scalable systems. Leads and mentors engineering teams (up to 5 members), ensuring compliance with industry standards and delivering efficient, high-quality results. Collaborates closely with stakeholders and product owners to meet business objectives, including delivering key projects for Capital One. Technically curious and committed to continuous learning, staying updated on industry trends, and implementing best practices to enhance security, availability, testability, and reliability. Skilled in technologies such as Java, Kotlin, Docker,CI/CD and AWS, focusing on optimizing system performance and managing projects from inception to delivery.</p>
 
       <p><strong>Principal Software Engineer, July 2022 – June 2024</strong><br/>
-      Spearheaded the development of new APIs, mentoring the team in best practices. Authored best practices documentation, reduced costs, and increased feature development for virtual card spending. Awarded for a creative solution affecting millions of customers.</p>
+      Led the team in developing new APIs, ensuring best practices during design and development. Mentored the team through PR reviews, pair programming, and tool demos. Impacted the technical roadmap by offering new solutions to reduce costs and maintenance. Authored a best practices document for coding, ensuring maintainability and observability. Collaborated with multiple teams to develop features that increased virtual card spending. Participated in workshops with UI and backend teams to find optimal solutions. Documented various applications to improve understanding and future enhancements. Won an organizational excellence award for a creative solution affecting millions of customers and significant revenue.</p>
       <p><strong>Senior Software Engineer, June 2021 – July 2022</strong><br/>
-      Designed a high-traffic API with low latency for virtual credit cards. Analyzed and improved system performance, implemented threading for efficiency, and utilized AWS for scalable deployment.</p>
+      Designed and delivered a high-traffic application (1000 TPS) with low latency for virtual credit cards. Analyzed an old version with high latency and error rates, removing unnecessary processes. Implemented threading for parallel card checks, maintaining performance levels. Deployed the new stack on AWS using scalable containers, reducing costs during low traffic. Designed a new DynamoDB for low latency data reuse.
+      </p>
       <p><strong>Stack:</strong> Java, Kotlin, Python, Springboot, Lambda, Fargate, EC2, DynamoDb, Rest Api's, New Relic, Splunk, K6, Karate</p>
     </div>
   );
